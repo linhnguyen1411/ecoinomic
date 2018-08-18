f50a166a431e0da541d65529ba8ef555977d6250
vannam1996 <dao.van.nam@framgia.com>
2018-08-18 10:49:19 +0700

    custom-login-register

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {registrations: "users/registrations",
+    sessions: "users/sessions"}
+
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
+    get "register", to: "devise/registrations#new"
+  end
 
   root to: "landing/landings#index", as: :user_root
-  scope module: 'landing' do
+  scope module: "landing" do
     resources :landings
   end
-  scope module: 'user' do
+  scope module: "user" do
     resources :users
     get "/faq", to: "faqs#index"
   end
